06f9fe2a928d7b99f0c4df1b93cc11a2d6ae964e
ELIEZER MUNUNGA <www.eliezer57@gmail.com>
2023-07-23 21:46:32 +0200

    u

--- view/js/script.js
+++ view/js/script.js
@@ -10,24 +10,3 @@ mediaButton.onclick = function () {
     mediaButton.classList.toggle("active");
     
 };
-
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
-
-
-/*const slidingNewsletter = document.querySelector('.home1');
-
-window.addEventListener('scroll', () => {
-
-    const {scrollTop, clientHeight} = document.documentElement;
-
-
-    const topElementToTopViewport = slidingNewsletter.getBoundingClientRect().top;
-
-    console.log(topElementToTopViewport);
-
-    if(scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.8){
-        slidingNewsletter.classList.add('active')
-    }
-})*/
